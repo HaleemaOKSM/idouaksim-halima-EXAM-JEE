43de00e8286ec3199e9b47a6fd257b805a372ceb
Halima <halimaouaksim@gmail.com>
2026-05-11 08:55:35 +0100

    first commit

---
+++ idouaksim-halima-Exam-JEE/src/test/java/halima/idouaksim/idouaksimhalimaexamjee/IdouaksimHalimaExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package halima.idouaksim.idouaksimhalimaexamjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdouaksimHalimaExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
